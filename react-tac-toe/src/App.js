f4cd3c88a05cc646381b8039af47f1aaadb05b55
Shavon Harris <Shavonharris9114@gmail.com>
2022-05-07 16:28:38 -0700

    first commit

---
+++ react-tac-toe/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+import Game from './components/Game'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className="App">
+    <Game />
+    <Footer className="footer"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-tac-toe/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      Created by Shavon 🤟🏿 2022
+    </div>
+  )
+}
+
+export default Footer
